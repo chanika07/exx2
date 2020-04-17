2fe6d1f2cb8c2dc0cb48c1828bae15d7eb5d23b2
chanika07 <chanika.ket@gmail.com>
2020-04-17 16:59:26 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+    int sum =0;
+    int sum1=0;
+	while(true){
+        int x = sc.nextInt();
+        if (x<=0)break;
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+         if (z == 0){
+            System.out.println(x*y);
+            sum=x*y;
+        }
+        else if (z==1){
+            System.out.println((x*y)+10);
+            sum1=(x*y)+10;
+        }System.out.println(sum+sum1);
+    }
+
+    }
+}
